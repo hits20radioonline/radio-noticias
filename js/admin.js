19c4bc8a1d52d1042d7642af71e2fab185406daa
hits20radioonline <hits20radioonline@gmail.com>
2026-08-16 03:43:26 -0300

    Update fetch URL and alert messages in admin.js

--- js/admin.js
+++ js/admin.js
@@ -8,15 +8,15 @@ document.getElementById('formNoticia').addEventListener('submit', function(e) {
     cuerpo: document.getElementById('cuerpo').value
   };
 
-  fetch("https://script.google.com/macros/s/AKfycbwz7XfGSu11ZwkP-HZ6J7v4kxSaXcnwaYOJvW1XGT6xDRB6aZMPn6GL8VZPcNXFJgWe/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbxz4jF6gAN35Myd69T745m8KyJPIf5-Ce0oZOzRFhGpMSctFl50pb8fB1CimuuBS-6S/exec", {
     method: 'POST',
     mode: 'no-cors',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(datosNoticia)
   })
   .then(() => {
-    alert('Noticia enviada');
+    alert('¡Noticia publicada con éxito!');
     document.getElementById('formNoticia').reset();
   })
-  .catch(err => alert('Error: ' + err));
+  .catch(err => alert('Error al enviar: ' + err));
 });
